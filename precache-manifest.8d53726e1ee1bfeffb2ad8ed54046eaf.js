220091225a634dc2fe002ec26875e5cb190f9050
Travis CI User <travis@example.org>
2019-01-09 08:11:05 +0000

    page

---
+++ precache-manifest.8d53726e1ee1bfeffb2ad8ed54046eaf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "778313300dbbd57161fa",
+    "url": "/ai.bundle.js"
+  },
+  {
+    "revision": "ebe55e6e94d8885856be",
+    "url": "/css/app.3fed5621.css"
+  },
+  {
+    "revision": "ebe55e6e94d8885856be",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "/img/board.5442f90f.jpg"
+  },
+  {
+    "revision": "ff2200d0ef44d9296684e4796733d748",
+    "url": "/img/win.ff2200d0.png"
+  },
+  {
+    "revision": "b8371a498233343eb9a64ba0fcec6fed",
+    "url": "/img/avatar.b8371a49.png"
+  },
+  {
+    "revision": "fa1aefb6062bf622d8aded7256e997b9",
+    "url": "/img/lose.fa1aefb6.png"
+  },
+  {
+    "revision": "4d05273ff4f4f00c7d6f1eb28d834fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8371a498233343eb9a64ba0fcec6fed",
+    "url": "/img/avatar.png"
+  },
+  {
+    "revision": "fa1aefb6062bf622d8aded7256e997b9",
+    "url": "/img/lose.png"
+  },
+  {
+    "revision": "ff2200d0ef44d9296684e4796733d748",
+    "url": "/img/win.png"
+  },
+  {
+    "revision": "39048e3000e0279beb4309bafbd73cfc",
+    "url": "/img/QRcode.png"
+  }
+];
